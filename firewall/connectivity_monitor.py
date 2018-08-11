d11ca69008fc5ec79ae47fd937582e304e55ec0c
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-08-11 21:33:43 +0200

    Make firewall service less talkative

--- firewall/connectivity_monitor.py
+++ firewall/connectivity_monitor.py
@@ -9,18 +9,17 @@ if __name__ == "__main__":
     # Give the system 5 minutes to start up
     print("Waiting 5 minutes before monitoring connection")
     time.sleep(5*60)
-    
+
+    print("Starting connectivity monitoring")
     while(True):
         response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
         
-        if response == 0:
-          print(hostname + ' is up!')
-        else:
-          print(hostname + ' is down!')
-          NetworkManager.NetworkManager.WirelessEnabled = False
-          time.sleep(5)
-          NetworkManager.NetworkManager.WirelessEnabled = True
-          time.sleep(50)
-          client.captureMessage('Network was down. WiFi connections have been restarted.')
+        if response != 0:
+            print(hostname + ' is down!')
+            NetworkManager.NetworkManager.WirelessEnabled = False
+            time.sleep(5)
+            NetworkManager.NetworkManager.WirelessEnabled = True
+            time.sleep(50)
+            client.captureMessage('Network was down. WiFi connections have been restarted.')
         
         time.sleep(10)
