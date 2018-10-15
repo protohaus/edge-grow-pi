e922ee0cdc9722a4293da72abf0c0675c5725e86
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-10-15 11:50:33 +0200

    Update the connectivity monitor to be more proactive
    - When a ping fails (google.com)
    - Will now disable Wifi, enable, search all connect Wifi devices
    - Apply the resin-wifi-01 connection to each one

--- firewall/connectivity_monitor.py
+++ firewall/connectivity_monitor.py
@@ -1,14 +1,46 @@
-import NetworkManager, time, os, raven
+import NetworkManager, time, os, raven, dbus
 
 # Host to ping to check connectivity
 hostname = "google.com"
 
 client = raven.Client(os.environ.get('SENTRY_DSN'))
 
+def activate_wifi_connection(name):
+    connections = NetworkManager.Settings.ListConnections()
+    connections = dict([(x.GetSettings()['connection']['id'], x) for x in connections])
+    conn = connections[name]
+    
+    ctype = conn.GetSettings()['connection']['type']
+    
+    # Check if the specified connection is a WiFi connection
+    if ctype == '802-11-wireless':
+        devices = NetworkManager.NetworkManager.GetDevices()
+        
+        # Go through all available WiFi devices and find unactivated ones
+        for dev in devices:
+            if(dev.DeviceType == NetworkManager.NM_DEVICE_TYPE_WIFI and
+                    dev.State != NetworkManager.NM_DEVICE_STATE_ACTIVATED):
+                # Try three times (Enable WiFi and then assign connection to device)
+                for i in range(3):
+                    try:
+                        NetworkManager.NetworkManager.WirelessEnabled = True
+                        time.sleep(5)
+                        NetworkManager.NetworkManager.ActivateConnection(conn, dev, "/")
+                        break
+                    except dbus.exceptions.DBusException:
+                        client.captureMessage('Failed to connect to WiFi. Try {}'.format(i))
+            
+                # Check if really connected
+                time.sleep(30)
+                if(dev.State != NetworkManager.NM_DEVICE_STATE_ACTIVATED):
+                    client.captureMessage('Failed to connect to {}'.format(name))
+    else:
+        client.captureMessage('Connection ({}) not a WiFi network ({})'.format(name, ctype))
+
 if __name__ == "__main__":
     # Give the system 5 minutes to start up
-    print("Waiting 5 minutes before monitoring connection")
-    time.sleep(5*60)
+    print("Waiting 1 minute before monitoring connection")
+    time.sleep(60)
 
     print("Starting connectivity monitoring")
     while(True):
@@ -16,10 +48,12 @@ if __name__ == "__main__":
         
         if response != 0:
             print(hostname + ' is down!')
+            
             NetworkManager.NetworkManager.WirelessEnabled = False
             time.sleep(5)
+            activate_wifi_connection('resin-wifi-01')
             NetworkManager.NetworkManager.WirelessEnabled = True
-            time.sleep(50)
+            time.sleep(60)
             client.captureMessage('Network was down. WiFi connections have been restarted.')
         
         time.sleep(10)
